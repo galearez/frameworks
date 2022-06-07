d6333ad2cdb24ec6d4e47ce933199d1e7d68db6d
Josep Galearez <51718252+galearez@users.noreply.github.com>
2022-06-06 20:22:08 -0600

    project setup using vite

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = `
+  <h1>Setup using Vite</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
+
+export {};
