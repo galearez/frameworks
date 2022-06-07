d89b80da3aa7ba84567b15817451e8a6de0e8871
Josep Galearez <51718252+galearez@users.noreply.github.com>
2022-06-06 20:29:31 -0600

    type annotations for create function

--- src/render.ts
+++ src/render.ts
@@ -1,7 +1,13 @@
+interface VNode {
+  nodeName: string;
+  attributes?: {};
+  children?: [string, VNode] | null;
+}
+
 // This function creates a single HTML node
-function create(nodeName: string, attributes: {}, ...args: any) {
+function create(nodeName: string, attributes: {}, ...args: any): VNode {
   let children = args.length
-    ? ([].concat(...args) as unknown as [string])
+    ? ([].concat(...args) as unknown as [string, VNode])
     : null;
 
   return { nodeName, attributes, children };
